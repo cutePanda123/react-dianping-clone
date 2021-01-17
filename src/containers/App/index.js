5868e7477689215306b799d9347d29d74d88e7e9
Peter Li <peterli1234@126.com>
2021-01-17 09:53:38 -0800

    encapusulate error handling component and actions

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import ErrorToast from "../../components/ErrorToast";
+import { getError, actions as appActions } from "../../redux/modules/app";
 import "./styles.css";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    const {
+      error,
+      appActions: { clearError },
+    } = this.props;
+    return (
+      <div className="App">
+        {error ? (
+          <ErrorToast msg={error} clearErrorHandler={clearError} />
+        ) : null}
+      </div>
+    );
   }
 }
 
-export default App;
+const mapStateToProps = (state, props) => {
+  return {
+    error: getError(state),
+  };
+};
+
+const mapDispatchTopProps = (dispatch) => {
+  return {
+    appActions: bindActionCreators(appActions, dispatch),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchTopProps)(App);
--- src/redux/modules/app.js
+++ src/redux/modules/app.js
@@ -1,5 +1,33 @@
-const reducer = (state = {}, action) => {
+import { act } from "react-dom/test-utils";
+
+const initialState = {
+    error: null
+};
+
+export const types = {
+    CLEAR_ERROR: 'APP/CLEAR_ERROR'
+};
+
+export const actions = {
+    clearError: () => ({
+        type: types.CLEAR_ERROR
+    })
+};
+
+const reducer = (state = initialState, action) => {
+    const { type, error } = action;
+    if (type === types.CLEAR_ERROR) {
+        return {...state, error: null};
+    }
+    if (error) {
+        return {...state, error: error};
+    }
     return state;
 }
 
 export default reducer;
+
+// use selectors to get state from redux to ducouple react ui from redux state
+export const getError = (state) => {
+    return state.app.error;
+}

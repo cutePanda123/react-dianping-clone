c58d079c27ecc2e2519053108e881f2cdef3e9f9
Peter Li <peterli1234@126.com>
2021-01-18 18:29:29 -0800

    implemented Headline with react-slick

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Category from "./components/Category";
+import Headline from './components/Headline';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Category />
+        <Headline />
       </div>
     );
   }

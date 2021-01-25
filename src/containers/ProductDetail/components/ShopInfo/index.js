74b944171b7534e2c430bc64b325380c2f6831a8
Peter Li <peterli1234@126.com>
2021-01-25 09:05:24 -0800

    implemented shop info component

---
+++ src/containers/ProductDetail/components/ShopInfo/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class ShopInfo extends Component {
+  render() {
+    return (
+      <div className="shopInfo">
+        <div className="shopInfo__header">
+          Shop count (4)
+          <span className="shopInfo__arrow"></span>
+        </div>
+        <div className="shopInfo__middle">
+          <div className="shopInfo__middleLeft">
+            <div className="shopInfo__shopName">
+              院落创意菜
+              <div className="shopInfo__starsWrapper">
+                <span className="shopInfo__stars">
+                  <i
+                    className="shopInfo__stars--red"
+                    style={{ width: "80%" }}
+                  ></i>
+                </span>
+                <span className="shopInfo__distance">100km</span>
+              </div>
+            </div>
+          </div>
+          <div className="shopInfo__middleRight">
+            <i className="shopInfo__phoneIcon"></i>
+          </div>
+        </div>
+        <div className="shopInfo__footer">
+          <i className="shopInfo__locationIcon"></i>Beijing
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShopInfo;
--- src/containers/ProductDetail/index.js
+++ src/containers/ProductDetail/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ProductOverview from './components/ProductOverview';
+import ShopInfo from './components/ShopInfo';
 
 class ProductDetail extends Component {
     render() {
         return (
             <div>
                 <ProductOverview />
+                <ShopInfo />
             </div>
         );
     }

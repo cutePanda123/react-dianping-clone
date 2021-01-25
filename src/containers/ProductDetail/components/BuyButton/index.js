38838aa21cab17d7e17ec08bd09ab9c37a6ef68f
Peter Li <peterli1234@126.com>
2021-01-25 11:37:28 -0800

    implemented buy button

---
+++ src/containers/ProductDetail/components/BuyButton/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class BuyButton extends Component {
+    render() {
+        return (
+            <a className='buyButton'>
+                Buy now
+            </a>
+        );
+    }
+}
+
+export default BuyButton;
